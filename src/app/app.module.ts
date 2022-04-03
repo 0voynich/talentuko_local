483d0fa1a2ee633ed8fee1bfa1f00db594f275ec
Omar Gtz <omar.gutierrez@vasslatam.com>
2022-04-03 13:05:29 -0500

    code base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './elements/header/header.component';
+import { FooterComponent } from './elements/footer/footer.component';
+import { SearchComponent } from './elements/search/search.component';
+import { ResultsComponent } from './results/results.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchComponent,
+    ResultsComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,

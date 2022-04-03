5153194573878ccbc3595be0d08ea0443c3e4f87
Omar Gtz <omar.gutierrez@vasslatam.com>
2022-04-03 13:26:33 -0500

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ResultsComponent } from './results/results.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component:HomeComponent },
+  { path:'results', component:ResultsComponent },
+  { path:'about', component:AboutComponent },
+  { path:'contact', component:ContactComponent },
+  { path:'**', pathMatch:'full', redirectTo:'' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
